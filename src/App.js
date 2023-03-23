2b3c9c5bd4947929924cb83eecd0dea77958361d
Jakub Bujak <kuba.bujak0@wp.pl>
2023-03-23 20:42:50 +0100

    Create and render random animal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+	const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+	return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [animals, setAnimals] = useState([]);
 
 	const handleClick = () => {
-		setCount(count + 1);
+		setAnimals([...animals, getRandomAnimal()]);
 	};
+
+	const renderedAnimals = animals.map((animal, index) => {
+		return <AnimalShow type={animal} key={index} />
+	})
  
 	return (
 		<div>
 			<button onClick={handleClick}>Add Animal</button>
-			<div>
-				Number of Animals: {count}
-			</div>
+			<div>{renderedAnimals}</div>
 		</div>
 	);
 }
