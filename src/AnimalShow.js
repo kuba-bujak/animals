85c0b8f309cfce3251805031423079e2c836788e
Jakub Bujak <kuba.bujak0@wp.pl>
2023-03-23 20:58:10 +0100

    Add heart image and handleClick to clicks

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
 import cow from './svg/cow.svg';
@@ -16,7 +17,22 @@ const svgMap = {
 }
 
 function AnimalShow({ type }) {
-	return <img  src={svgMap[type]} alt='animal' />
+	const [clicks, setClicks] = useState(0)
+
+	const handleClick = () => {
+		setClicks(clicks + 1);
+	};
+
+	return (
+		<div onClick={handleClick}>
+			<img  src={svgMap[type]} alt='animal' />
+			<img 
+				src={heart} 
+				alt='heart' 
+				style={{ width: 10 + 10 * clicks + 'px'}}
+			/>
+		</div>
+	);
 }
 
 export default AnimalShow;
