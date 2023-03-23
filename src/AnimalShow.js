e751b5fa729bd56cb8390b804d5cd7e02ea0f9b5
Jakub Bujak <kuba.bujak0@wp.pl>
2023-03-23 20:50:53 +0100

    display svg

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,22 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+import heart from './svg/heart.svg';
+
+const svgMap = {
+	bird,
+	cat,
+	cow,
+	dog,
+	gator,
+	horse
+}
+
 function AnimalShow({ type }) {
-	return <div>{type}</div>
+	return <img  src={svgMap[type]} alt='animal' />
 }
 
 export default AnimalShow;
